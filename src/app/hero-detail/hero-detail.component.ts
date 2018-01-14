5eccf35bc5f8c9549ca8ca4dce39918d0573d310
katsuyuki.mori <mori1103k@gmail.com>
2018-01-14 23:45:11 +0900

    done: services

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../heroes/hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+  heroes: Hero[] = [];
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+}

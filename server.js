62a79ec54c42c26df8283138fbc4d0e447886491
Katsuyuki Mori <katsuyuki.mr@gmail.com>
2018-01-29 07:24:08 +0900

    +

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-
-//Install express server
 const express = require('express');
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-// app.get('/*', function(req, res){
-//     res.sendFile(path.join(__dirname + 'dist/index.html'));
-// })
+const path = require('path');
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+console.log('Server listening on port 8080');
